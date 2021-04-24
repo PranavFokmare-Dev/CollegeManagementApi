47a95a435df0d65b22ecc787400347bc71c730a1
pranav fokmare <pfokmare1@gmail.com>
2021-04-24 17:04:44 +0530

    When I generated the controller for reference purposes it changed the version of entity framework related packages

--- CollegeManagementApi/Services/IStudentRepository.cs
+++ CollegeManagementApi/Services/IStudentRepository.cs
@@ -10,6 +10,14 @@ namespace CollegeManagementApi.Services
     public interface IStudentRepository
     {
         Task<IEnumerable<Student>> GetStudents();
+        Task<bool> AddStudent(Student student);
+        Task<Student> GetStudentById(int id);
+        Task UpdateStudent(Student s);
+        Task DeleteStudent(Student s);
+        bool StudentExists(int id);
+        bool TeacherExists(int id);
+        Task<Teacher> GetProctorByStudentId(int sid);
+        Task SetProctor(int sid, int pid);
     }
 
     public class StudentRepository : IStudentRepository
@@ -21,7 +29,88 @@ namespace CollegeManagementApi.Services
         }
         public async Task<IEnumerable<Student>> GetStudents()
         {
-            return await _context.Students.Include(p=>p.Degree).Include(p=>p.School).ToListAsync();
+            return await _context.Students.ToListAsync();
+        }
+
+        public async Task<bool> AddStudent(Student student)
+        {
+            if(await checkDegree(student.DegreeId) && await checkSchool(student.SchoolId))
+            {
+                await _context.Students.AddAsync(student);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            return false;
+        }
+        private async Task<bool> checkDegree(int? id)
+        {
+            Degree degree =await _context.Degrees.FindAsync(id);
+            if (degree == null)
+                return false;
+            return true;
+        }
+
+        private async Task<bool> checkSchool(int? id)
+        {
+            School s = await _context.Schools.FindAsync(id);
+            if (s == null)
+                return false;
+            return true;
+        }
+        
+        public async Task<Student> GetStudentById(int id)
+        {
+            Student s = await _context.Students.Include(s => s.Degree).Include(s => s.School).Include(s => s.Proctor).Include(s => s.Proctor.Designation).Include(s => s.Proctor.School).Where(s => s.StudentId == id).FirstOrDefaultAsync();
+            return s;
+        }
+
+        
+
+        public async Task UpdateStudent(Student student)
+        {
+            if (!StudentExists(student.StudentId))
+                throw new InvalidOperationException($"Student doesnt exist with id {student.StudentId} to update");
+
+            _context.Entry(student).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteStudent(Student student)
+        {
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Teacher> GetProctorByStudentId(int sid)
+        {
+            if (!StudentExists(sid))
+                return null;
+            Student s = await _context.Students.Include(s=>s.Proctor).Where(s=>s.StudentId == sid).FirstOrDefaultAsync();
+            return s.Proctor;
+        }
+
+
+        public async Task SetProctor(int sid,int tid)
+        {
+            Student student = await _context.Students.FindAsync(sid);
+            Teacher proctor = await _context.Teachers.FindAsync(tid);
+            student.ProctorId = tid;
+            student.Proctor = proctor;
+            _context.Entry(student).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+
+        }
+
+
+
+        public bool StudentExists(int id)
+        {
+            return _context.Students.Any(e => e.StudentId == id);
+        }
+        public bool TeacherExists(int id)
+        {
+            return _context.Teachers.Any(e => e.TeacherId == id);
         }
     }
 }
